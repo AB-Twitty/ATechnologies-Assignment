d500196fdbda46846b993b27f43df943589fbd34
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2025-02-25 20:58:30 +0200

    Base generic repository

---
+++ ATechnologiesAssignment.App/Contracts/IRepositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using ATechnologiesAssignment.App.Helpers;
+using ATechnologiesAssignment.Domain.Base;
+using System.Linq.Expressions;
+
+namespace ATechnologiesAssignment.App.Contracts.IRepositories
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<PagedList<TEntity>> GetPaginatedAsync(int pageIndex, int pageSize);
+        Task<PagedList<TEntity>> GetPaginatedAsync(int pageIndex, int pageSize, Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> GetByIdAsync(string id);
+        Task<bool> AddAsync(TEntity entity);
+        Task<TEntity> AddWithReturnAsync(TEntity entity);
+        void DeleteByIdAsync(string id);
+    }
+}
---
+++ ATechnologiesAssignment.App/Helpers/PagedList.cs
@@ -0,0 +1,20 @@
+﻿namespace ATechnologiesAssignment.App.Helpers
+{
+    public class PagedList<TData>
+    {
+        public IEnumerable<TData> Data { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+        public PagedList(IEnumerable<TData> data, int pageIndex, int pageSize, int totalCount)
+        {
+            Data = data;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+        }
+    }
+}
